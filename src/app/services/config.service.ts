155d6402db737747a0ca9e53a35f4597a5c305b4
antoniaalbu <antoniaxv@gmail.com>
2025-01-16 14:56:14 +0200

    sidebar
    Sidebar code

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 export interface MenuItem {
   name: string;
   route: string;
@@ -14,20 +13,26 @@ export interface FooterConfig {
   transparent: boolean;
 }
 
+export interface SidebarConfig {
+  enabled: boolean;
+  submenu: { [key: string]: string[] };  // Submenu where key is the route and value is an array of submenu items
+}
+
 export interface Config {
   menu: MenuItem[];
   header: {
     sticky: boolean;
     transparent: boolean;
   };
-  footer: FooterConfig; 
+  footer: FooterConfig;
+  sidebar: SidebarConfig;  // Sidebar updated to reflect submenu structure
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
-  private configUrl = 'menu-config.json';
+  private configUrl = 'menu-config.json';  // Make sure this path is correct
 
   constructor(private http: HttpClient) {}
 
